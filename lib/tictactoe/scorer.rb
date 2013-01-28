4c54d60b6c737d65e6c0265d487c523cd5010bec
Andrew Kelly <kellyaj@gmail.com>
2013-01-28 14:32:39 -0600

    slicker win check

--- lib/tictactoe/scorer.rb
+++ lib/tictactoe/scorer.rb
@@ -7,7 +7,7 @@ class Scorer
 
   def is_won?(board)
     winner = false
-    winner = true if  board.horizontal_rows.collect {|row| is_won_row?(row)}.include?(true) || board.vertical_rows.collect {|row| is_won_row?(row)}.include?(true) || board.diagonal_rows.collect {|row| is_won_row?(row)}.include?(true)
+    winner = true if board.horizontal_rows.concat(board.vertical_rows).concat(board.diagonal_rows).any? {|row| is_won_row?(row)}
     winner
   end
 
