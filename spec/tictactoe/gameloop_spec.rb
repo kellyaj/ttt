968ee61696b98ca874205ed72bbd747b8b8e9539
Andrew Kelly <kellyaj@gmail.com>
2013-01-31 13:57:04 -0600

    removed extranneous tests

--- spec/tictactoe/gameloop_spec.rb
+++ spec/tictactoe/gameloop_spec.rb
@@ -12,22 +12,4 @@ describe GameLoop do
   #   game_loop.main
   # end
 
-  # it "should be able to start the game" do
-  #   output.should_receive(:puts).with('Tic-Tac-Toe Time!')
-  #   output.should_receive(:puts).with('Choose a space to occupy.')
-  #   game_loop.start_game
-  # end
-
-  # it "should be able to get a player move" do
-  #   game_loop.make_player_move.should be_true
-  # end
-
-  # it "should know if the game is over" do
-  #   game_loop.is_over?.should be_false
-  # end
-
-  # it "should be able to cycle players" do
-  #   game_loop.cycle_players.should be_true
-  # end
-
 end
