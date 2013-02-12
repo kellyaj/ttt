724926fd6fc5b5b2117a5522c5edca239be613bc
Andrew Kelly <kellyaj@gmail.com>
2013-02-12 15:57:28 -0600

    full game playing features complete

--- features/step_definitions/full_game_being_played.rb
+++ features/step_definitions/full_game_being_played.rb
@@ -10,6 +10,10 @@ class Output
 
 end
 
+def output
+    @output ||= Output.new
+end
+
 # computer vs computer game
 
 Given /^that there are two computer players$/ do
@@ -24,11 +28,11 @@ end
 # human vs human game
 
 Given /^that there are two human players$/ do
-  @human_game_loop = GameLoop.new(output, StringIO.new("human\nhuman"))
-  @human_game_loop.assign_players()
+  @human_game_loop = GameLoop.new(output, StringIO.new("human\nhuman\nno"))
+  @human_game_loop.game = Game.new(output, [1, 2, 3, 4, 5, 6, 7, 8, 9], @input, Player.new(Human.new(output, StringIO.new("1\n2\n3\nno"))), Player.new(Human.new(output, StringIO.new("4\n5\n6\nno"))))
   @human_game_loop.start_game
 end
 
 Then /^we should be able to play until the end\.$/ do
-  @human_game_loop.is_over?.should == true
+  @human_game_loop.game.is_over?.should == true
 end
