f60f920279faa0e66710a581f35a2be91d5ab0ff
Andrew Kelly <kellyaj@gmail.com>
2013-02-20 15:36:24 -0600

    testing, table setup

--- webttt.rb
+++ webttt.rb
@@ -2,12 +2,19 @@ require 'sinatra'
 $:.unshift(File.expand_path("../lib/", __FILE__))
 require 'tictactoe.rb'
 
+
+use Rack::Session::Pool
+
 get '/' do
+  session[:name] ||= "Andrew"
   erb :index
 end
 
 
 get '/game' do
-  @board = Board.new([1,2,3,4,5,6,7,8,9], STDOUT)
+  @name = session[:name]
+  session[:board] ||=  Board.new([1,2,3,4,5,6,7,8,9], STDOUT)
+  @board = session[:board]
+  @positions = @board.positions
   erb :game
 end
