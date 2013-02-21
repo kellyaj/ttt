ebc3a007aec4e3bc5866defabe11016ab4e4cde0
Andrew Kelly <kellyaj@gmail.com>
2013-02-21 09:31:48 -0600

    session properly working

--- webttt.rb
+++ webttt.rb
@@ -1,20 +1,24 @@
 require 'sinatra'
 $:.unshift(File.expand_path("../lib/", __FILE__))
 require 'tictactoe.rb'
-
-
+class Something < Sinatra::Base
 use Rack::Session::Pool
 
-get '/' do
-  session[:name] ||= "Andrew"
-  erb :index
-end
+  get '/' do
+    erb :index
+  end
 
 
-get '/game' do
-  @name = session[:name]
-  session[:board] ||=  Board.new([1,2,3,4,5,6,7,8,9], STDOUT)
-  @board = session[:board]
-  @positions = @board.positions
-  erb :game
+  get '/game' do
+    session[:board] ||=  Board.new([1,2,3,4,5,6,7,8,9], STDOUT)
+    @board = session[:board]
+    @positions = @board.positions
+    erb :game
+  end
+
+  get '/make_move' do
+    redirect '/game'
+  end
 end
+
+Something.run!
