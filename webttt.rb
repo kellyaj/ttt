d20f6cddabf47ab2b73524ace35f28f55c70485e
Andrew Kelly <kellyaj@gmail.com>
2013-02-21 10:17:10 -0600

    two computers can play a game with a human manually clicking place move

--- webttt.rb
+++ webttt.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'sinatra'
 $:.unshift(File.expand_path("../lib/", __FILE__))
 require 'tictactoe.rb'
@@ -12,13 +13,22 @@ class WebTicTacToe < Sinatra::Base
 
 
   get '/game' do
-    session[:board] ||=  Board.new([1,2,3,4,5,6,7,8,9], STDOUT)
+    player1 = Player.new(Computer.new("X"))
+    player2 = Player.new(Computer.new("O"))
+    session[:game] ||= Game.new($stdout, [1,2,3,4,5,6,7,8,9], $stdin, player1, player2)
+    @game = session[:game]
+    session[:board] ||=  @game.board
     @board = session[:board]
-    @positions = @board.positions
+    @spots = @board.positions
     erb :game
   end
 
   get '/make_move' do
+    game = session[:game]
+    current_player = game.current_player
+    board = session[:board]
+    board.place_move(game.get_player_move, current_player.mark)
+    game.cycle_players
     redirect '/game'
   end
 end
